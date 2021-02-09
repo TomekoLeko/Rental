b73da9522a1f23b0fe4da1127b2237504addc096
TomekoLeko <ays@vp.pl>
2021-02-09 15:38:14 +0100

    feat: residing tenants

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -19,13 +19,13 @@ class RentsController < ApplicationController
   def new
     @rent = Rent.new
     @properties = current_user.properties.where(rented: false)
-    @tenants = current_user.tenants
+    @tenants = current_user.tenants.where(resident: false)
   end
 
   # GET /rents/1/edit
   def edit
     @properties = current_user.properties.where(id: @rent.property_id)
-    @tenants = current_user.tenants
+    @tenants = current_user.tenants.where(id: @rent.tenant_id)
   end
 
   # POST /rents
@@ -33,7 +33,9 @@ class RentsController < ApplicationController
   def create
     @rent = Rent.new(rent_params)
     property = current_user.properties.find_by_id(@rent.property_id)
+    tenant = current_user.tenants.find_by_id(@rent.tenant_id)
     property.update_attribute(:rented, true)
+    tenant.update_attribute(:resident, true)
     respond_to do |format|
       if @rent.save
         format.html { redirect_to @rent, notice: 'Rent was successfully created.' }
@@ -49,7 +51,9 @@ class RentsController < ApplicationController
   # PATCH/PUT /rents/1.json
   def update
     property = current_user.properties.find_by_id(@rent.property_id)
+    tenant = current_user.tenants.find_by_id(@rent.tenant_id)
     property.update_attribute(:rented, true)
+    tenant.update_attribute(:resident, true)
     respond_to do |format|
       if @rent.update(rent_params)
         format.html { redirect_to @rent, notice: 'Rent was successfully updated.' }
@@ -65,7 +69,9 @@ class RentsController < ApplicationController
   # DELETE /rents/1.json
   def destroy
     property = current_user.properties.find_by_id(@rent.property_id)
+    tenant = current_user.tenants.find_by_id(@rent.tenant_id)
     property.update_attribute(:rented, false) if property
+    tenant.update_attribute(:resident, false) if tenant
     @rent.destroy
     respond_to do |format|
       format.html { redirect_to rents_url, notice: 'Rent was successfully destroyed.' }
