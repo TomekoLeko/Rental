7b26822f3080cbac1dc49d29bb142119f413a3d0
TomekoLeko <ays@vp.pl>
2021-02-17 11:07:25 +0100

    feat: blocked deleting residing tenants

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -59,10 +59,14 @@ class TenantsController < ApplicationController
   # DELETE /tenants/1
   # DELETE /tenants/1.json
   def destroy
+    if @tenant.resident
+      redirect_to tenants_path, alert: 'You cannot delete. This tenant is residing in a property.'
+    else  
     @tenant.destroy
     respond_to do |format|
       format.html { redirect_to tenants_url, notice: 'Tenant was successfully deleted.' }
       format.json { head :no_content }
+    end  
     end
   end
 
